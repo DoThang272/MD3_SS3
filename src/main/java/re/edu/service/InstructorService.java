e0a853732c16dd1150df87ce8248050a6c98a5b0
DoThang <doanhthang16@gmail.com>
2026-04-02 12:23:44 +0700

    gio1

--- src/main/java/re/edu/service/InstructorService.java
+++ src/main/java/re/edu/service/InstructorService.java
@@ -2,6 +2,7 @@ package re.edu.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import re.edu.dto.InstructorCreateRequest;
 import re.edu.model.Instructor;
 import re.edu.repository.InstructorRepository;
 
@@ -9,14 +10,26 @@ import java.util.List;
 
 @Service
 public class InstructorService {
-//    private final InstructorRepository instructorRepository;
-//
-//    @Autowired
-//    public InstructorService(InstructorRepository instructorRepository) {
-//        this.instructorRepository = instructorRepository;
-//    }
-//
-//    public List<Instructor> findAll(){
-//        return instructorRepository.findAll();
-//    }
+    private final InstructorRepository instructorRepository;
+
+    @Autowired
+    public InstructorService(InstructorRepository instructorRepository) {
+        this.instructorRepository = instructorRepository;
+    }
+
+    public Instructor findInstructorById(Long id){
+        return instructorRepository.findById(id).orElseThrow(() -> new RuntimeException("Không tìm thấy giảng viên"));
+    }
+
+    public List<Instructor> finAllInstructor(){
+        return instructorRepository.findAll();
+    }
+
+    public Instructor createInstructor(InstructorCreateRequest req){
+        Instructor instructor = new Instructor();
+        instructor.setName(req.getName());
+        instructor.setEmail(req.getEmail());
+
+        return instructorRepository.save(instructor);
+    }
 }
